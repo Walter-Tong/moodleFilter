5b88f86ec12a3928f08aa5f785100cc4893a3a2e
Walter <walter19@connect.hku.hk>
2024-01-21 00:28:04 +0800

    handle issue find in the add drop period

--- scripts/filter.js
+++ scripts/filter.js
@@ -17,35 +17,73 @@ async function filter() {
         }
     });
 
+    let prevCourseBox = [];
+
+    await chrome.storage.sync.get("courseBox", function (result) {
+        const storedCourses = result.courseBox;
+        if (storedCourses) {
+            prevCourseBox = storedCourses;
+        }
+    })
+
+    let prevSideBar = [];
+
+    await chrome.storage.sync.get("sideBarCourses", function (result) {
+        const storedCourses = result.sideBarCourses;
+        if (storedCourses) {
+            prevSideBar = storedCourses;
+        }
+    })
+
+    await new Promise(resolve => setTimeout(resolve, 250));
+
     const courseNameList = [];
 
-    for (let i = 0; i < courses.length; i++) {
-        courseNameList.push(courses[i].getElementsByClassName("coursename")[0].children[0].innerText.substring(0, 8))
+    if (window.location.href === 'https://moodle.hku.hk/' ) {
+        for (let i = 0; i < courses.length; i++) {
+            const courseName = courses[i].getElementsByClassName("coursename")[0].children[0].innerText.substring(0, 8)
+    
+            if (!prevCourseBox.includes(courseName)) {
+                console.log(courseName)
+                whiteListBox.push(courseName)
+            }
+    
+            courseNameList.push(courseName)
+        }
     }
 
     const sideBarCourseNameList = [];
 
     for (let i = 0; i < sideBarCourses.length; i++) {
+        const courseName = sideBarCourses[i].innerText.substring(0, 8)
+
+        if (!prevSideBar.includes(courseName)) {
+            console.log(courseName)
+            whiteListSide.push(courseName)
+        }
+
         sideBarCourseNameList.push(sideBarCourses[i].innerText.substring(0, 8))
     }
 
     await new Promise(resolve => setTimeout(resolve, 250));
 
-    await chrome.storage.sync.set({ boxCourses: whiteListBox, sideCourses: whiteListSide, courseBox: courseNameList, sideBarCourses: sideBarCourseNameList });
+    await chrome.storage.sync.set({ boxCourses: whiteListBox, sideCourses: whiteListSide, courseBox: (courseNameList.length ? courseNameList : prevCourseBox) , sideBarCourses: (sideBarCourseNameList.length ? sideBarCourseNameList : prevSideBar) });
 
     console.log(whiteListBox, whiteListSide)
 
     await new Promise(resolve => setTimeout(resolve, 250));
     let offset = 0;
 
-    if (whiteListBox.length) {
-        let timeToRun = courses.length
-
-        for (let i = 0; i < timeToRun; i++) {
-            const courseName = courses[i - offset].getElementsByClassName("coursename")[0].children[0].innerText.substring(0, 8);
-            if (!whiteListBox.includes(courseName)) {
-                courses[i - offset].remove();
-                offset++;
+    if (window.location.href === 'https://moodle.hku.hk/' ) {
+        if (whiteListBox.length) {
+            let timeToRun = courses.length
+    
+            for (let i = 0; i < timeToRun; i++) {
+                const courseName = courses[i - offset].getElementsByClassName("coursename")[0].children[0].innerText.substring(0, 8);
+                if (!whiteListBox.includes(courseName)) {
+                    courses[i - offset].remove();
+                    offset++;
+                }
             }
         }
     }
